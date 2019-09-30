dad38b4cc460a9403d20f968c65c2e122d6036ed
Muhamad Fahdy <fahdy173040030@mail.unpas.ac.id>
2019-09-30 21:26:50 +0700

    mengubah nilai properti di class Produk

--- produk.php
+++ produk.php
@@ -5,12 +5,19 @@
 // Game
 
 class Produk{
-	public $judu = "judul",
+	public $judul = "judul",
 		   $penulis = "penulis",
 		   $penerbit =  "penerbit",
 		   $harga = "0";
 
 }
 
-$Produk1 = new Produk();
-var_dump($Produk1);
+// $Produk1 = new Produk();
+// var_dump($Produk1);
+
+$Produk2 = new Produk();
+$Produk2->judul = "Naruto";
+$Produk2->penulis = "Masashi Kishimoto";
+$Produk2->penerbit = "Shonen Jump";
+$Produk2->harga = "30000";
+var_dump($Produk2);
