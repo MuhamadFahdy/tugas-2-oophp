7d078c8f22cf8afd3ba0630cb224e0cbfbc99fcd
Muhamad Fahdy <fahdy173040030@mail.unpas.ac.id>
2019-10-01 21:34:27 +0700

    membuat file dengan nama inheritance-problem.php

---
+++ inheritance-problem.php
@@ -0,0 +1,55 @@
+<?php 
+
+// Jualan Produk
+// Komik
+// Game
+
+class Produk{
+	//properti
+	public $judul ,
+		   $penulis ,
+		   $penerbit ,
+		   $harga;
+
+	//Method
+	public function __construct( $judul = "judul", $penulis = "penulis", $penerbit =  "penerbit", $harga = 0){
+		$this->judul = $judul;
+		$this->penulis = $penulis;
+		$this->penerbit = $penerbit;
+		$this->harga = $harga;	
+	}
+
+	//Method
+	public function getLabel() {
+		return "$this->penulis,$this->penerbit";
+	}
+
+}
+
+class CetakInfoProduk{
+	public function cetak( Produk $Produk){
+		$str = "{$Produk->judul} | {$Produk->getLabel()} (Rp. {$Produk->harga})";
+		return $str; 
+
+	}
+}
+
+//menambahkan produk2
+$Produk2 = new Produk("Naruto","Masashi Kishimoto", "Shoen Jump", 30000);
+
+
+//menambahkan produk3
+$Produk3 = new Produk("Uncharted","Neil Duckmann","Sony Computer",250000);
+
+
+//echo untuk menampilkan ke layar
+echo "Komik : ". $Produk2->getLabel();
+echo "<br>";
+echo "Game : " . $Produk3->getLabel();
+echo "<br>";
+
+//instansiasi
+$infoProduk1 = new CetakInfoProduk();
+echo $infoProduk1->cetak($Produk2);
+
+
