9314dc87a09f7189dec55dd84238f9eb1fe49640
Muhamad Fahdy <fahdy173040030@mail.unpas.ac.id>
2019-10-01 21:49:11 +0700

    memambahkan parameter baru di produk 2 dan produk3

--- inheritance-problem.php
+++ inheritance-problem.php
@@ -14,7 +14,7 @@ class Produk{
 		   $waktuMain;
 
 	//Method
-	public function __construct( $judul = "judul", $penulis = "penulis", $penerbit =  "penerbit", $harga = 0){
+	public function __construct( $judul = "judul", $penulis = "penulis", $penerbit =  "penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0){
 		$this->judul = $judul;
 		$this->penulis = $penulis;
 		$this->penerbit = $penerbit;
@@ -37,11 +37,11 @@ class CetakInfoProduk{
 }
 
 //menambahkan produk2
-$Produk2 = new Produk("Naruto","Masashi Kishimoto", "Shoen Jump", 30000);
+$Produk2 = new Produk("Naruto","Masashi Kishimoto", "Shoen Jump", 30000, 100);
 
 
 //menambahkan produk3
-$Produk3 = new Produk("Uncharted","Neil Duckmann","Sony Computer",250000);
+$Produk3 = new Produk("Uncharted","Neil Duckmann","Sony Computer",250000, 50);
 
 
 //echo untuk menampilkan ke layar
