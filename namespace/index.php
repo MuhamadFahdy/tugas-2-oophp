ffd42902b645138a98b675a9eea9e8df10c16944
Muhamad Fahdy <fahdy173040030@mail.unpas.ac.id>
2019-10-02 23:12:01 +0700

    membuat file namespace duplikat dari file autoloading

---
+++ namespace/index.php
@@ -0,0 +1,19 @@
+<?php 
+
+require_once "App/init.php";
+
+//menambahkan produk2
+$Produk2 = new Komik("Naruto","Masashi Kishimoto", "Shoen Jump", 30000, 100);
+
+
+//menambahkan produk3
+$Produk3 = new Game("Uncharted","Neil Duckmann","Sony Computer",250000, 50);
+
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk($Produk2);
+$cetakProduk->tambahProduk($Produk3);
+
+echo $cetakProduk->cetak();
+
+
+ ?>
