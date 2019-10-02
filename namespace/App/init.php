4b3dab680222fa39c9b71868ca978c6aad3ae739
Muhamad Fahdy <fahdy173040030@mail.unpas.ac.id>
2019-10-02 23:42:17 +0700

    membuat nameSpace class user di folder Produk dan service

--- namespace/App/init.php
+++ namespace/App/init.php
@@ -1,4 +1,4 @@
-<?php 
+ <?php 
 
 // require_once "Produk/InfoProduk.php";
 // require_once "Produk/Produk.php";
@@ -8,6 +8,15 @@
 
 //melakukan fungsi autoload
 spl_autoload_register(function($class){
+	$class = explode("\\", $class);
+	$class = end($class);
 	require_once __DIR__."/Produk/".$class . ".php";
-});
+		});
+
+spl_autoload_register(function($class){
+	$class = explode("\\", $class);
+	$class = end($class);
+	require_once __DIR__."/Service/".$class . ".php";
+	});
+
  ?>
--- namespace/index.php
+++ namespace/index.php
@@ -2,18 +2,23 @@
 
 require_once "App/init.php";
 
-//menambahkan produk2
-$Produk2 = new Komik("Naruto","Masashi Kishimoto", "Shoen Jump", 30000, 100);
+// //menambahkan produk2
+// $Produk2 = new Komik("Naruto","Masashi Kishimoto", "Shoen Jump", 30000, 100);
 
 
-//menambahkan produk3
-$Produk3 = new Game("Uncharted","Neil Duckmann","Sony Computer",250000, 50);
+// //menambahkan produk3
+// $Produk3 = new Game("Uncharted","Neil Duckmann","Sony Computer",250000, 50);
 
-$cetakProduk = new CetakInfoProduk();
-$cetakProduk->tambahProduk($Produk2);
-$cetakProduk->tambahProduk($Produk3);
+// $cetakProduk = new CetakInfoProduk();
+// $cetakProduk->tambahProduk($Produk2);
+// $cetakProduk->tambahProduk($Produk3);
 
-echo $cetakProduk->cetak();
+// echo $cetakProduk->cetak();
 
+use App\Service\User as ServiceUser;
+use App\Produk\User as ProdukUser;
 
+new  ServiceUser();
+echo "<hr>";
+new ProdukUser();
  ?>
